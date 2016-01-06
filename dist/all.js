86466a9de8d21ff6456f62b20d25e0c8f09ed212
Jay Place <jay.place@methodfactory.com>
2016-01-06 15:23:30 -0500

    Changes from client review

--- dist/all.js
+++ dist/all.js
@@ -188,8 +188,10 @@ $(document).ready(function(){
 });
 $(document).ready(function(){
 
+    console.log("The DOM is ready");
 
-    // Toggle Login
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Toggle login input box
     $(".login-toggle").click(function(){
         // console.log(e.target);
         $(this).addClass("open");
@@ -198,8 +200,9 @@ $(document).ready(function(){
     });
 
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Topggle search input box
 
-    // Topggle Search
     $(".search__button").click(function(){
         $(".search-container").addClass("open");
         $(".search__button").addClass("open");
@@ -208,34 +211,9 @@ $(document).ready(function(){
     });
 
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Mobile nav dropdown toggle
 
-
-
-    // var bodyHeight = $("body").height();
-    // var setBodyHeight = function() {
-    //     $(".container.page--stripes").height(bodyHeight);
-    // };
-    // setBodyHeight();
-
-
-    // Set width of Mobile Nav panels
-    var panelWidth = $(window).width()-30;
-    var setPanel = function(){
-        if (panelWidth < 780) {
-            $(".nav-level-2").width(panelWidth);
-            $(".nav-level-3").width(panelWidth);
-        }
-        else if (panelWidth >= 780) {
-        }
-    };
-    setPanel();
-
-
-
-
-
-
-    // Toggle Mobile Nav
     $(".nav-small-icon").click(function(){
         if ($(this).hasClass("open")) {
             $(this).removeClass("open");
@@ -254,16 +232,25 @@ $(document).ready(function(){
     });
 
 
-    // var navID = $(this).attr("ID");
-    // var dropID = $(".nav-dropdown ." + navID);
-    // var level1 = $(".nav-level-1");
-    // var level2 = $(".nav-level-2");
-    // var level3 = $(".nav-level-3");
-    // var link = $(".nav__list--item a");
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Set width of mobile nav panels
 
+    var panelWidth = $(window).width()-30;
+    var setPanel = function(){
+        if (panelWidth < 780) {
+            $(".nav-level-2").width(panelWidth);
+            $(".nav-level-3").width(panelWidth);
+        }
+        else if (panelWidth >= 780) {
+        }
+    };
+    setPanel();
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Mobile nav sliding panes
 
     var mobileNav = function(){
-
         var navID = $(this).attr("ID");
         var dropContainer = $(".nav-dropdown");
         var dropID = $(".nav-dropdown ." + navID);
@@ -272,13 +259,12 @@ $(document).ready(function(){
         var level3 = $(".nav-level-3");
         var link = $(".nav__list--item a");
 
-        $(dropID).addClass("open");
-
-        if (panelWidth >= 780) {
+        if (panelWidth >= 480) {
             $(level1).addClass("open");
             $(dropContainer).addClass("open");
+            $(dropID).addClass("open");
         }
-        if (panelWidth < 780) {
+        else if (panelWidth < 480) {
             if ($(level1).hasClass("open")) {
                 $(dropContainer).addClass("open");
                 $(dropID).addClass("open");
@@ -289,116 +275,18 @@ $(document).ready(function(){
                 $(level2).addClass("left");
                 $(level3).addClass("open");
             }
-
-            // $(".nav-main").addClass("left");
-            // $(level1).addClass("open");
-            // $("dropID").addClass("open");
         }
     };
 
     $(".nav__list--item a").click(mobileNav);
-
     $(".exit-level-2").click(function() {
         $(".nav-level-2").removeClass("open");
         $(".nav-level-1").removeClass("left");
     });
 
 
-    // $('.some-class').click(slideonlyone('sms_box'));
-    // $(".nav-level-1 .nav__list--item a").click(function(){
-    //     console.log($(this).attr("ID"));
-    //     // mobileNav();
-    // });
-
-    // $(".nav-level-1 .nav__list--item a").click( function(navID){
-    //     var navID = $(this).attr("ID");
-    // });
-
-    // say your selector and click handler looks something like this...
-    // $(".nav-level-1 .nav__list--item a").click({param1: "Hello", param2: "World"}, cool_function);
-
-
-
-    // $(".nav-level-1 .nav__list--item a").click(function(){
-    //     var navID = $(this).attr("ID");
-    //     var dropID = $(".nav-dropdown ." + navID);
-    //     var level1 = $(".nav-level-1");
-    //     var level2 = $(".nav-level-2");
-    //     var level3 = $(".nav-level-3");
-    //     var link = $(".nav__list--item a");
-
-    //     $(dropID).addClass("open");
-
-    //     if (panelWidth >= 780) {
-    //         $(level1).addClass("")
-    //     }
-    //     if (panelWidth < 780) {
-    //         // console.log("Mobile View");
-    //         console.log(navID);
-    //         console.log(dropID);
-    //         $(".nav-main").addClass("left");
-    //         $(".nav-dropdown").addClass("open");
-    //         $("dropID").addClass("open");
-    //     }
-    // });
-
-
-
-
-    // $("#bowlers").click(function(){
-    //     if (panelWidth >= 780) {
-    //         $(".bowlers").addClass("open");
-    //     }
-    //     if (panelWidth < 780) {
-    //         $(".nav-main").addClass("left");
-    //         $(".nav-dropdown").addClass("open");
-    //         $(".bowlers").addClass("open");
-    //     }
-    // });
-
-    // $(".exit--bowlers").click(function() {
-    //     $(".nav-dropdown").removeClass("open");
-    //     $(".bowlers").removeClass("open");
-    //     $(".nav-main").removeClass("left").addClass("open");
-    // });
-
-    // $("#youth").click(function(){
-    //     $(".nav-main").addClass("left");
-    //     $(".nav-dropdown").addClass("open");
-    //     $(".youth").addClass("open");
-    // });
-
-    // $(".exit--youth").click(function() {
-    //     $(".nav-dropdown").removeClass("open");
-    //     $(".youth").removeClass("open");
-    //     $(".nav-main").removeClass("left").addClass("open");
-    // });
-
-    // $("#youth-tournaments").click(function(){
-    //     // $(".nav-main").addClass("left");
-    //     // $(".nav-dropdown").addClass("open");
-    //     $(".youth").removeClass("open").addClass("left");
-    //     $(".youth-tournaments").addClass("open");
-    // });
-
-    // $(".exit--youth-tournaments").click(function() {
-    //     $(".nav-dropdown").removeClass("left").removeClass("open");
-    //     $(".youth").removeClass("left").addClass("open");
-    //     $(".youth-tournaments").removeClass("open");
-    //     $(".nav-main").removeClass("left").addClass("open");
-    // });
-
-
-
-
-
-
-
-
-
-
-
-    // Close Login and Search on Click Outside
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Close Login and Search on click outside
     $(document).mouseup(function(e)
     {
         var dropdown = $(".nav-dropdown");
@@ -409,20 +297,20 @@ $(document).ready(function(){
 
         if(e.target.id != dropdown.attr('id') && !dropdown.has(e.target).length)
         {
-            // alert("Outside Dropdown");
+            // console.log("Outside Dropdown");
             $(".dropdown-wrapper").removeClass("open");
         }
         if(e.target.id != loginDropdown.attr('id') && !loginDropdown.has(e.target).length)
         {
             // console.log("Login Button");
-            // alert("Outside Login");
+            // console.log("Outside Login");
             // console.log(e.target);
             $(loginDropdown).removeClass("open");
             $(loginToggle).removeClass("open");
         }
         if(e.target.id != hamburger.attr('id') && hamburger.has(e.target).length)
         {
-            // alert("Outside Hamburger");
+            // console.log("Outside Hamburger");
             // $(".dropdown-wrapper").removeClass("open");
             // $(".search__input").val("");
             // $(".search-container").removeClass("open");
@@ -433,7 +321,7 @@ $(document).ready(function(){
         }
         if(e.target.id != search.attr('id') && !search.has(e.target).length)
         {
-            // alert("Outside Search");
+            // console.log("Outside Search");
             $(".search__input").val("");
             $(search).removeClass("open");
             $(".search__button").removeClass("open");
@@ -444,20 +332,17 @@ $(document).ready(function(){
     });
 
 
-
-
-
-
-
-
-    // Marketing Promo Sliding Cover
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Marketing promo slide over
     $(".marketing-promo").hover(function(){
         $(".marketing-promo__detail").toggleClass("open");
     });
 
 
-
-    // SlickJS plugin; slick() is called on the nav.
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Slider plugin and parameters, .slick() is called
+    // on 'slider--foreground' and 'slider--background',
+    // which are synced by plugin setting, asNavFor.
 
     $(".slider--foreground").slick({
         infinite: true,
@@ -473,7 +358,6 @@ $(document).ready(function(){
         autoplaySpeed: 215000,
         pauseOnHover: true
     });
-
     $('.slider--background').slick({
         infinite: true,
         // autoplay: true,
@@ -485,7 +369,7 @@ $(document).ready(function(){
         arrows: false,
         pauseOnHover: true
     });
-
+    // Change slider stripe color based on data-attribute of individual content element.
     var stripeBlue = $(".slider--stripes .stripe").attr("data-stripeColor") === "blue";
     var stripeYellow = $(".slider--stripes .stripe").attr("data-stripeColor") === "yellow";
     var slideBlue = $(".slider--foreground .slide").attr("data-slideColor") === "blue";
@@ -510,17 +394,6 @@ $(document).ready(function(){
 
 
 
-
-
-    // !!!! For Preview Only !!!! Background Color Change Event
-    $(".cta__button").click(function() {
-        $(".cta__background").toggleClass("green");
-    });
-
-
-
-
-
     // Set Fixed Height for Article Rows with Ads
         // $(window).load(function() {
         //     var width = $('.article.fixed-height').width();
@@ -528,7 +401,7 @@ $(document).ready(function(){
         // });
 
 
-
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Equal Height for Articles
         // these are (ruh-roh) globals. You could wrap in an
         // immediately-Invoked Function Expression (IIFE) if you wanted to...
@@ -568,25 +441,30 @@ $(document).ready(function(){
             });
         }
         $(window).bind("load", function() {
-            columnConform();
+            // columnConform();
+            console.log("Everything is loaded");
         });
+
+        setTimeout(function(){
+            console.log("Time's up");
+            console.log("Now run the column equalizer...");
+            columnConform();
+        }, 5000);
+
+        // $(window).load(function () {
+        //     columnConform();
+        // });
+
         $(window).resize(function() {
             columnConform();
         });
 
-
         // Dom Ready
         // You might also want to wait until window.onload if images are the things that
         // are unequalizing the blocks
-
-        $(function() {
-            columnConform();
-        });
-
-
-    // $(".top-news__summary").dotdotdot({
-    //     // configuration goes here
-    // });
+        // $(function() {
+        //     columnConform();
+        // });
 
 
 });
