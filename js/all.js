f7b643af0fba2df2fe9606a6e6a5a3a0a3f1095d
Jay Place <jay.place@methodfactory.com>
2016-01-21 20:17:37 -0500

    Moved dropdown close to it's own module.

--- js/all.js
+++ js/all.js
@@ -183,38 +183,7 @@ $(window).bind("load", function() {
 
 
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Close Login and Search on click outside
-$(document).mouseup(function(e)
-{
-    var dropdown = $(".nav-dropdown");
-    var loginToggle = $(".login-toggle");
-    var searchToggle = $(".search__toggle");
-    var loginDropdown = $(".login-wrapper");
-    var search = $(".search-container");
 
-    if(e.target.id != dropdown.attr('id') && !dropdown.has(e.target).length)
-    {
-        $(".nav__list--item a").removeClass("active");
-        $(".dropdown-wrapper").removeClass("open");
-    }
-    if(e.target.id != loginDropdown.attr('id') && !loginDropdown.has(e.target).length)
-    {
-        $(loginDropdown).removeClass("open");
-        $(loginToggle).removeClass("open");
-    }
-    if(e.target.id != search.attr('id') && !search.has(e.target).length)
-    {
-        $(".search__input").val("");
-        $("search").removeClass("open");
-        $(".search__button").removeClass("open");
-        $(".search__input").css("display", "none");
-        $(".search__input--wrapper").removeClass("open");
-        $(".search-container").removeClass("open");
-        $(".login-wrapper").removeClass("open");
-        $(".login-toggle").removeClass("open");
-    }
-});
 $(window).bind("load", function() {
     $(".sidebar-links__title").click(function (){
         console.log("click");
@@ -343,3 +312,35 @@ $(window).bind("load", function() {
         arrows: false
     });
 // });
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Close Login and Search on click outside
+$(document).mouseup(function(e)
+{
+    var dropdown = $(".nav-dropdown");
+    var loginToggle = $(".login-toggle");
+    var searchToggle = $(".search__toggle");
+    var loginDropdown = $(".login-wrapper");
+    var search = $(".search-container");
+
+    if(e.target.id != dropdown.attr('id') && !dropdown.has(e.target).length)
+    {
+        $(".nav__list--item a").removeClass("active");
+        $(".dropdown-wrapper").removeClass("open");
+    }
+    if(e.target.id != loginDropdown.attr('id') && !loginDropdown.has(e.target).length)
+    {
+        $(loginDropdown).removeClass("open");
+        $(loginToggle).removeClass("open");
+    }
+    if(e.target.id != search.attr('id') && !search.has(e.target).length)
+    {
+        $(".search__input").val("");
+        $("search").removeClass("open");
+        $(".search__button").removeClass("open");
+        $(".search__input").css("display", "none");
+        $(".search__input--wrapper").removeClass("open");
+        $(".search-container").removeClass("open");
+        $(".login-wrapper").removeClass("open");
+        $(".login-toggle").removeClass("open");
+    }
+});
