0a4739927d389acfaca58112036b7f5d6e237ffb
Jay Place <jay.place@methodfactory.com>
2016-02-02 23:44:51 -0500

    Commented out -- prevent trimming whole word

--- js/all.js
+++ js/all.js
@@ -247,16 +247,20 @@ $(".sidebar-links__title").click(function(){
     var areWeOnMobile = function() {
         return $('#mobile-menu-toggle').is(":visible");
     };
-
-    var cta3Tiles = [$(".top-news__summary p"),$(".marketing-promo__summary p")];
-    var newsTiles = $(".article__summary");
-
     if(areWeOnMobile()) {
+        var cta3Tiles = [$(".top-news__summary p"),$(".marketing-promo__summary p")];
+        var newsTiles = $(".article__summary");
+        // var newsTileFull = $(newsTiles).text();
+        // var newsTileTrimmed = newsTileFull.substr(0, 130);
+        // newsTileTrimmed = newsTileTrimmed.substr(0, Math.min(newsTileTrimmed.length, newsTileTrimmed.lastIndexOf(" ")))
+
         $(cta3Tiles).each(function(){
             $(this).text($(this).text().substr(0,80)).addClass("visible");
         });
+
         $(newsTiles).each(function(){
-            $(this).text($(this).text().substr(0,150));
+            $(this).text($(this).text().substr(0,140));
+            // $(this).text(newsTileTrimmed);
         });
     }
 
