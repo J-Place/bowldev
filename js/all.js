cfee91e46ee163277bbc30e6ab6e16f09e3bb081
Jay Place <jay.place@methodfactory.com>
2016-02-15 17:39:16 -0500

    Youth page quick links, slider setup

--- js/all.js
+++ js/all.js
@@ -285,24 +285,14 @@ $(".sidebar-links__title").click(function(){
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // slick() is called on 'slider--foreground' and 'slider--background',
-// which are synced via setting 'asNavFor'.
-
-// Change slider stripe color based on data-attribute of individual content element.
-var slideBlue = $(".slider--foreground .slide").attr("data-slideColor") === "blue";
-var slideYellow = $(".slider--foreground .slide").attr("data-slideColor") === "yellow";
-var slideOrange = $(".slider--foreground .slide").attr("data-slideColor") === "orange";
-var firstSlide = $(".slider .slide").first().attr("data-slideColor");
-// var slideColor = $(this).attr("data-slideColor");
-var slideColor = $(".slider--foreground .slide").attr("data-slideColor");
+// which are synced by setting 'asNavFor'.
 
 $(".slider--foreground").on({ 'afterChange': function(event, slick, currentSlide){
-    // console.log("after" + currentSlide);
-    $(".slider__ribbon .ribbon").toggleClass("blue");
-    $(".slider--stripes .stripes").toggleClass("blue");
+    $(".slider__ribbon .ribbon").toggleClass("blue-youth");
     $(".slider__ribbon .ribbon").toggleClass("yellow");
+    $(".slider--stripes .stripes").toggleClass("blue-youth");
     $(".slider--stripes .stripes").toggleClass("yellow");
-}
-}).slick({
+}}).slick({
     infinite: true,
     autoplay: true,
     asNavFor: '.slider--background',
