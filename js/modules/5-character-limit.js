0defc4875fb8963d7db150a2b1127b10724a5ca0
Jay Place <jay.place@methodfactory.com>
2016-02-18 11:25:55 -0500

    Separated variables for Top News and Marketing Promo paragraphs

--- js/modules/5-character-limit.js
+++ js/modules/5-character-limit.js
@@ -4,24 +4,18 @@
 
 (function(){
 
-    // var areWeOnMobile = function() {
-    //     return $('#mobile-menu-toggle').is(":visible");
-    // };
-    // if(areWeOnMobile()) {
-        var cta3Tiles = [$(".top-news__summary p"),$(".marketing-promo__summary p")];
-        var newsTiles = $(".article__summary");
-        // var newsTileFull = $(newsTiles).text();
-        // var newsTileTrimmed = newsTileFull.substr(0, 130);
-        // newsTileTrimmed = newsTileTrimmed.substr(0, Math.min(newsTileTrimmed.length, newsTileTrimmed.lastIndexOf(" ")))
+    var topNewsChar = $(".top-news__summary p");
+    var mktgPromoChar = $(".marketing-promo__summary p")
+    var newsTiles = $(".article__summary");
 
-        $(cta3Tiles).each(function(){
-            $(this).text($(this).text().substr(0,80)).addClass("visible");
-        });
-
-        $(newsTiles).each(function(){
-            $(this).text($(this).text().substr(0,140));
-            // $(this).text(newsTileTrimmed);
-        });
-    // }
+    $(topNewsChar).each(function(){
+        $(this).text($(this).text().substr(0,80)).addClass("visible");
+    });
+    $(mktgPromoChar).each(function(){
+        $(this).text($(this).text().substr(0,80)).addClass("visible");
+    });
+    $(newsTiles).each(function(){
+        $(this).text($(this).text().substr(0,140));
+    });
 
 })();
